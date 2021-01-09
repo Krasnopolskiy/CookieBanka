ea6062ef79aa4994b5584fc37ff6f1d5caf92fc6
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-01-09 22:03:30 +0300

    Remove sanitize function

--- app/misc.py
+++ app/misc.py
@@ -1,13 +1,3 @@
-from string import ascii_letters, digits
-
-
-def sanitize(s: str) -> str:
-    alpha = ascii_letters + digits + '_-'
-    res = ''.join([ch if ch in alpha else '' for ch in s])
-    return res
-
-
 def load_template_string(template: str) -> str:
-    template = sanitize(template)
     with open(f'app/templates/{template}.html', 'r') as f:
         return f.read()
